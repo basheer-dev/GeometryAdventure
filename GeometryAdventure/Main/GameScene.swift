7e6983f4c0b3e783b095239057dde27cf145ad9c
Basheer Abdulmalik <basheer0abdulmalik@gmail.com>
2024-01-25 18:58:09 +0200

    Code clean up

--- GeometryAdventure/Main/GameScene.swift
+++ GeometryAdventure/Main/GameScene.swift
@@ -11,14 +11,9 @@ class GameScene: SKScene {
     
     private let tileMap = TileMapManager.shared.getTileMap(fileNamed: "tilemap2")
     private let player = Player(rectOf: CGSize(width: 32, height: 64))
-    private let tileMapSurface = SKNode()
     private let cameraNode = SKCameraNode()
-    private var terrains: [SKShapeNode] = []
     
     override func didMove(to view: SKView) {
-//        let activeMap = Array(map.prefix(30)).map { $0.prefix(40) }
-//        let activeMap = map[1...2].map { $0[3...5] }
-//        player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 32, height: 64))
         player.draw(in: self, position: CGPoint(x: frame.width / 2, y: frame.height / 2))
         camera = cameraNode
         
@@ -45,41 +40,7 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-//        removeChildren(in: terrains)
-//        terrains.removeAll(keepingCapacity: true)
-        
         cameraNode.position = player.position
-        
-//        let centerRowID = abs(Int((player.position.y - frame.height) / 32))
-//        let centerColumnID = abs(Int(player.position.x / 32))
-//        
-//        let startingRowID = centerRowID - 15 > 0 ? centerRowID - 15 : 0
-//        let endingRowID = centerRowID + 15 < tileMap.count - 1 ? centerRowID + 15 : tileMap.count - 1
-//        
-//        let startingColumnID = centerColumnID - 20 > 0 ? centerColumnID - 20 : 0
-//        let endingColumnID = centerColumnID + 20 < tileMap[0].count - 1 ? centerColumnID + 20 : tileMap[0].count - 1
-//        
-//        if startingRowID < endingRowID - 1 && startingColumnID < endingColumnID - 1 {
-//            let activeMap = tileMap[startingRowID...endingRowID - 1].map { $0[startingColumnID...endingColumnID - 1] }
-//            print("\(startingRowID) | \(endingRowID)")
-//            
-//            for (rowID, row) in activeMap.enumerated() {
-//                for (columnID, column) in row.enumerated() {
-//                    if column == 0 {
-//                        let terrain = SKShapeNode(rectOf: CGSize(width: 32, height: 32))
-//                        terrain.physicsBody = SKPhysicsBody(rectangleOf: terrain.frame.size)
-//                        terrain.physicsBody?.isDynamic = false
-//                        terrain.fillColor = .red
-//                        terrain.strokeColor = .red
-//                        
-//                        terrain.position = CGPoint(x: (startingColumnID + columnID) * 32,
-//                                                   y: (startingRowID + rowID) * 32)
-//                        terrains.append(terrain)
-//                        addChild(terrain)
-//                    }
-//                }
-//            }
-//        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
